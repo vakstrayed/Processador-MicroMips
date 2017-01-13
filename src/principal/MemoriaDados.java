274bfc1d5bd41b4f960890f8f1147b0dcb3507a0
Mateus <a.mateuslol@gmail.com>
2017-01-13 17:16:11 -0300

    implementando método de inserção SB

--- src/principal/MemoriaDados.java
+++ src/principal/MemoriaDados.java
@@ -49,16 +49,16 @@ public class MemoriaDados {
 		int casa = posicao * 4;
 		// casa vai dizer quem que byte deve ser inserido
 		casa = end - casa;
-		
+
 		int chave = verificaENDERECO(Integer.toString(TOposicao));
-		
+
 		String dado = Integer.toBinaryString(valor);
-		
+
 		String getdado = this.memoria.get(chave).getConteudo();
 		String dataBIN = Integer.toBinaryString(Integer.parseInt(getdado));
 		dataBIN = util.completacomZero(dataBIN);
-		
-		String db,dg,dataINSERT;
+
+		String db, dg, dataINSERT;
 
 		switch (casa) {
 		case 0:
@@ -85,9 +85,37 @@ public class MemoriaDados {
 		}
 
 	}
-	
-	public void InserirSB(){
+
+	public void InserirSB(String endereco, int valor) {
+
+		int end = Integer.parseInt(endereco);
+		end = end - posicINICIO;
+		int posicao = end / 4;
+		// TOposicao diz em que endereço ele deve ser inserido
+		int TOposicao = posicINICIO + posicao;
+		int casa = posicao * 4;
+		// casa vai dizer quem que byte deve ser inserido
+		casa = end - casa;
+
+		int chave = verificaENDERECO(Integer.toString(TOposicao));
+
+		String dado = Integer.toBinaryString(valor);
+
+		String getdado = this.memoria.get(chave).getConteudo();
+		String dataBIN = Integer.toBinaryString(Integer.parseInt(getdado));
+		dataBIN = util.completacomZero(dataBIN);
 		
+		switch (casa) {
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		}
+
 	}
 
 }
